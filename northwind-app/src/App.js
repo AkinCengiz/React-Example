9dc77abddc976585b6ec9b9a9ede7bb980e1b386
akincengiz <akincengiz@msn.com>
2024-10-24 16:50:27 +0300

    Router created

--- northwind-app/src/App.js
+++ northwind-app/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import ProductList from './components/productList/ProductList';
 import Home from './pages/Home';
 import Categories from './pages/Categories';
 import Orders from './pages/Orders';
 import Products from './pages/Products';
+import Layout from './components/Layout';
+
 
 export default class App extends Component {
   render() {
     return (
       <Router>
+        <Layout />
         <Routes>
-          <Route path="/" element={<ProductList />} />
+          <Route path="/" element={<Layout />} />
           <Route index element={<Home/>} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/orders" element={<Orders />} />
